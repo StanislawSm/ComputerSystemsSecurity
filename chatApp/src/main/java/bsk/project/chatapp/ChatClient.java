938da8ff7f5405d2598afeb4cf31784f34e96b89
StanislawSm <105853049+StanislawSm@users.noreply.github.com>
2023-06-18 19:28:18 +0200

    add login screen

--- chatApp/src/main/java/bsk/project/chatapp/ChatClient.java
+++ chatApp/src/main/java/bsk/project/chatapp/ChatClient.java
@@ -1,41 +1,22 @@
 package bsk.project.chatapp;
-import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ChatClient extends Application{
 
     @Override
     public void start(Stage stage) throws IOException {
         //loading a scene from fxml file
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainWindowView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 480, 480);
-        //gathering controller Object from loaded scene
-        MainWindowController controller = fxmlLoader.getController();
-
-        //this latch will be used to synchronize two threads: one creating socket connection and other setting output stream for Main window controller
-        CountDownLatch latch = new CountDownLatch(1);
-
-        //Creating client handler with the controller gathered before and the latch
-        ClientHandler clientHandler = new ClientHandler(latch, controller);
-        Thread thread = new Thread(clientHandler);
-        thread.start();
-
-        //Here we have to wait for the clientHandler thread to establish a connection with other app, because we need output stream created in that thread
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        //finally we have an output stream, and we can use it in the main window controller
-        controller.setOutStream(clientHandler.getOutStream());
+        FXMLLoader loginFxmlLoader = new FXMLLoader(ChatServer.class.getResource("loginClientWindowView.fxml"));
+        Scene loginScene = new Scene(loginFxmlLoader.load(), 480, 480);
 
         stage.setTitle("chatApp1.0 Client");
-        stage.setScene(scene);
+        stage.setScene(loginScene);
         stage.show();
     }
 
---
+++ chatApp/src/main/java/bsk/project/chatapp/LoginServerWindowController.java
@@ -0,0 +1,54 @@
+package bsk.project.chatapp;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+public class LoginServerWindowController {
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label label;
+
+    @FXML
+    protected void onLoginButtonClick() throws IOException {
+        Stage stage = (Stage) passwordField.getScene().getWindow();
+
+
+        //compare passwords etc.
+        //loading a scene from fxml file
+        FXMLLoader fxmlLoader = new FXMLLoader(ChatServer.class.getResource("mainWindowView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 480, 480);
+        //gathering controller Object from loaded scene
+        MainWindowController controller = fxmlLoader.getController();
+
+        //this latch will be used to synchronize two threads: one creating socket connection and other setting output stream for Main window controller
+        CountDownLatch latch = new CountDownLatch(1);
+
+        //Creating client handler with the controller gathered before and the latch
+        ServerHandler serverHandler = new ServerHandler(latch, controller);
+        Thread thread = new Thread(serverHandler);
+        thread.start();
+
+        //Here we have to wait for the clientHandler thread to establish a connection with other app, because we need output stream created in that thread
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        //finally we have an output stream, and we can use it in the main window controller
+        controller.setOutStream(serverHandler.getOutStream());
+
+        stage.setTitle("chatApp1.0 Server");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
